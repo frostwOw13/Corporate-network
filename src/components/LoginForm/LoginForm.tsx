3e7de607c3cb33bcb1a2371fd9698128de8f0388
frostwOw13 <frostwow13@yandex.ru>
2021-08-11 12:18:12 +0300

    feat: add all login and register forms

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,34 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './LoginForm.scss';
 
-const LoginForm: React.FC = () => (
-  <div className="wrapper">
-    <div id="formContent">
-      <h2 className="active"> Sign In </h2>
-      <h2 className="inactive">Sign Up </h2>
+const LoginForm: React.FC = () => {
+  const [mode, setMode] = useState<string>('login');
 
-      <form>
-        <input
-          className="first"
+  const switchMode = (e: React.MouseEvent<HTMLElement>) => {
+    const target = e.target as Element;
+    setMode(target.id);
+  };
+
+  return (
+    <div className="wrapper">
+      <div id="formContent">
+        <h2
+          aria-hidden="true"
+          className={mode === 'login' ? 'active' : 'inactive'}
           id="login"
-          name="login"
-          placeholder="login"
-          type="text"
-        />
-        <input
-          className="second"
-          id="password"
-          name="login"
-          placeholder="password"
-          type="text"
-        />
-        <input className="third" type="submit" value="Log In" />
-      </form>
+          onClick={switchMode}
+        >
+          Sign In
+        </h2>
+        <h2
+          aria-hidden="true"
+          className={mode === 'login' ? 'inactive' : 'active'}
+          id="signup"
+          onClick={switchMode}
+        >
+          Sign Up
+        </h2>
 
-      <div id="formFooter" />
+        {mode === 'login' ? (
+          <form>
+            <input
+              className="first"
+              id="login"
+              name="login"
+              placeholder="username"
+              type="text"
+            />
+            <input
+              className="second"
+              id="password"
+              name="login"
+              placeholder="password"
+              type="text"
+            />
+            <input className="third" type="submit" value="Log In" />
+          </form>
+        ) : (
+          <form>
+            <input
+              className="first"
+              id="signup"
+              name="signup"
+              placeholder="username"
+              type="text"
+            />
+            <input
+              className="second"
+              id="password"
+              name="signup"
+              placeholder="password"
+              type="text"
+            />
+            <input
+              className="third"
+              id="confirmPassword"
+              name="signup"
+              placeholder="confirm password"
+              type="text"
+              value=""
+            />
+            <input className="fourth" type="submit" value="Sign Up" />
+          </form>
+        )}
+
+        <div id="formFooter" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoginForm;
