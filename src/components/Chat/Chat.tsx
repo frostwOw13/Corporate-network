dfe59cd5def0a03dfba1ec130bff27230382ab6a
frostwOw13 <frostwow13@yandex.ru>
2021-08-11 19:11:14 +0300

    feat: add switcher between chats

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,30 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import ChatFlood from './ChatFlood/ChatFlood';
 import ChatWork from './ChatWork/ChatWork';
 import './Chat.scss';
 
-const Chat = () => (
-  <div className="chat__container">
-    <div className="chat__wrapper">
-      <div className="chat__contacts">
-        <div className="contact__switcher">
-          <div className="switcher"><h2>Work Chat</h2></div>
-          <div className="switcher"><h2>Spam Chat</h2></div>
+const Chat = () => {
+  const [chat, setChat] = useState<string>('work');
+
+  const chatHandler = (e: React.MouseEvent<HTMLElement>) => {
+    const target = e.target as Element;
+    setChat(target.id);
+  };
+
+  return (
+    <div className="chat__container">
+      <div className="chat__wrapper">
+        <div className="chat__contacts">
+          <div className="contact__switcher">
+            <div
+              aria-hidden="true"
+              className={
+                chat === 'work' ? 'switcher active' : 'switcher inactive'
+              }
+              id="work"
+              onClick={chatHandler}
+            >
+              <h2 id="work">Work</h2>
+            </div>
+            <div
+              aria-hidden="true"
+              className={
+                chat === 'flood' ? 'switcher active' : 'switcher inactive'
+              }
+              id="flood"
+              onClick={chatHandler}
+            >
+              <h2 id="flood">flood</h2>
+            </div>
+          </div>
+          <ul className="contacts__list">
+            <li className="contacts__item">
+              <div className="contacts__image" />
+              admin1
+            </li>
+            <li className="contacts__item">
+              <div className="contacts__image" />
+              admin2
+            </li>
+          </ul>
         </div>
-        <ul className="contacts__list">
-          <li className="contacts__item">
-            <div className="contacts__image" />
-            admin1
-          </li>
-          <li className="contacts__item">
-            <div className="contacts__image" />
-            admin2
-          </li>
-        </ul>
+        {chat === "work" ? (
+          <ChatWork />
+        ) : (
+          <ChatFlood />
+        )}
       </div>
-      <ChatWork />
     </div>
-  </div>
-);
+  );
+};
 
 export default Chat;
---
+++ src/components/Chat/ChatFlood/ChatFlood.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChatFlood = () => (
+  <div className="chat__box">
+    <h2 className="chat__title">Flood Chat</h2>
+  </div>
+);
+
+export default ChatFlood;
